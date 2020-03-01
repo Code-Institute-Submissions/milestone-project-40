6e4691adafed60f181f5458982f8edded7c897b3
Paul Lucey <paulinnorway@gmail.com>
2020-03-01 12:45:34 +0100

    Define Kid object

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
+/***************************************************************** OBJECTS */
 //define Kindergarten object (constructor function)
 function Kindergarten(date, kidsAsleep, kidsAwake) {
   this.date = date; //todays date
   this.kidsAsleep = kidsAsleep; //total number of kids currently asleep
   this.kidsAwake = kidsAwake; //total number of kids currently awake
   this.kids = []; //array of Kid objects
+}
+
+//define Kid object (constructor function)
+function Kid(
+  name,
+  putDownTime,
+  sleepStartTime,
+  sleepStopTime,
+  takeUpTime,
+  sleepDuration
+) {
+  this.name = name;
+  this.putDownTime = putDownTime; //time when kid was put in pram
+  this.sleepStartTime = sleepStartTime; //time when kid fell asleep
+  this.sleepStopTime = sleepStopTime; //time when kid woke up
+  this.takeUpTime = takeUpTime; //time when kid was taken out of pram
+  this.sleepDuration = sleepDuration; //total time kid was sleeping
 }
