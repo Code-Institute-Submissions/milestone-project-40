8231646af72bd765babb70d1f9492a30e5227720
Paul Lucey <paulinnorway@gmail.com>
2020-03-16 16:02:36 +0100

    Delete Kindergarten object

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
 /***************************************************************** OBJECTS */
-//define Kindergarten object (constructor function)
-function Kindergarten(date, kidsAsleep, kidsAwake) {
-  this.date = date; //todays date
-  this.kidsAsleep = kidsAsleep; //total number of kids currently asleep
-  this.kidsAwake = kidsAwake; //total number of kids currently awake
-  this.kids = []; //array of Kid objects
-}
 //define Kid object (constructor function)
 function Kid(
   name,
