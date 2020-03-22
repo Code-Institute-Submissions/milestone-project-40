2318795e9631cb64fb148e720c8f974ebb14bba6
Paul Lucey <paulinnorway@gmail.com>
2020-03-22 12:42:53 +0100

    Fix variable name to todayDateFormatted

--- js/scripts.js
+++ js/scripts.js
@@ -102,7 +102,7 @@ function getHeaderData() {
       <div class="col-4 col-md-2">
         <div class="row text-center">
           <div class="col-12">Date:</div>
-          <div class="col-12">${todayFormatted}</div>
+          <div class="col-12">${todayDateFormatted}</div>
         </div>
       </div>
       <div class="col-4 col-md-2">
