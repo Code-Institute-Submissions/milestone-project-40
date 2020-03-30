78760bc08766a823b593277137fab4ce5c7d2dcb
Paul Lucey <paulinnorway@gmail.com>
2020-03-30 16:54:26 +0200

    Add activeDepartment.kidsAsleep to getHeaderData()

--- js/scripts.js
+++ js/scripts.js
@@ -226,7 +226,7 @@ function getHeaderData() {
       <div class="col-4 col-md-2">
         <div class="row text-center">
           <div class="col-12">Asleep:</div>
-          <div class="col-12">0</div>
+          <div class="col-12">${activeDepartment.kidsAsleep}</div>
         </div>
       </div>
     </div>
