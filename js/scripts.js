4c5f5daf63103934c7239056645c52921e218aae
Paul Lucey <paulinnorway@gmail.com>
2020-03-30 17:12:06 +0200

    Decrement kidsAsleep in takeUp()

--- js/scripts.js
+++ js/scripts.js
@@ -116,6 +116,7 @@ Kid.prototype.takeUp = function(event) {
   this.sleepDuration = Math.floor(
     (this.sleepStopTime - this.sleepStartTime) / 60000
   );
+  activeDepartment.kidsAsleep--;
   console.log("==>> Slept for (mins): ", this.sleepDuration);
   activeDepartment.refreshList();
 };
