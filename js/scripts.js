f7c86e6ff8d7ee79aeac14739a93fbbd01151e5d
Paul Lucey <paulinnorway@gmail.com>
2020-04-05 21:18:34 +0200

    takeUp() as call back function

--- js/scripts.js
+++ js/scripts.js
@@ -228,11 +228,9 @@ $("#listContainer").on("click", ".actionIcon", activeDepartment, function (
     activeDepartment.kids[index].asleepYet();
   } else if (statusText == `${activeDepartment.kids[index].name} is asleep`) {
     console.log("<- takeUp() ->");
-    let awake = parseInt($("#appSummaryData").find(".awake-total").text();
-    console.log("awake-total", awake);
-    //  let asleep = ;
-    $(`#listContainer ul li:nth-child(${index + 1})`).slideUp();
-    activeDepartment.kids[index].takeUp();
+    $(`#listContainer ul li:nth-child(${index + 1})`).slideUp(function () {
+      activeDepartment.kids[index].takeUp();
+    });
   }
 }); //jQuery event listener
 
