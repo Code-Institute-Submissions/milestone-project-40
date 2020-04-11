fc9a4e8f89ec07c6d8c7ab81572493e5cc81d740
Paul Lucey <paulinnorway@gmail.com>
2020-04-11 22:49:23 +0200

    Remove uneccessary console.logs

--- jasmine-testing/spec/scripts.spec.js
+++ jasmine-testing/spec/scripts.spec.js
@@ -8,12 +8,10 @@ activeDepartment.kids.push(new Kid("John", 1));
 for (const kid of activeDepartment.kids) {
   names.push(kid.name);
 };
-console.log("**** STARTING PUTDOWN SECTION ****");
-//************************************************************** putDown ******************** */
+//****************************************************** putDown() method ******************** * /
 describe(`Kid object > putDown() method:`, function () {
   beforeEach(function () {
     if (activeDepartment.kids.length == 0) { //if there are no kids, you must push them into kids array
-      //   console.log("--need to import kids before starting");
       activeDepartment.kids.push(new Kid("Tina", 4));
       activeDepartment.kids.push(new Kid("Jane", 3));
       activeDepartment.kids.push(new Kid("Mathew", 2));
@@ -28,20 +26,16 @@ describe(`Kid object > putDown() method:`, function () {
     //https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript
     activeDepartment.kids.length = 0; //empty array
   });
-  //*****************************************************************************************************function */
+  //*********************************************************************************function */
   function testPutDown(originalNameIndex) {
     it(`${names[originalNameIndex]}'s properties should equal expected result`, function () {
-      //arrange
       //https://www.codementor.io/@junedlanja/copy-javascript-object-right-way-ohppc777d
       let testObject = JSON.parse(JSON.stringify(activeDepartment));
       let originalName = names[originalNameIndex];
       let resultNameIndex = "";
       let passed = false;
-      //act
 
-      console.log("call putDown() using " + originalName);
       activeDepartment.kids[originalNameIndex].putDown();
-
       //get new nameIndex after list was sorted by putDown()
       for (let index = 0; index < activeDepartment.kids.length; index++) {
         if (originalName == activeDepartment.kids[index].name) {
@@ -49,8 +43,8 @@ describe(`Kid object > putDown() method:`, function () {
         }
       }
 
-      //assert
       let timeDiff = new Date() - activeDepartment.kids[resultNameIndex].putDownTime;
+      //check if all conditions are true to pass test
       if (activeDepartment.kids[resultNameIndex].status == `${names[originalNameIndex]} awake in pram`
         && activeDepartment.kids[resultNameIndex].statusImg == `emoji_baby_awake_in_pram.png`
         && activeDepartment.kids[resultNameIndex].action == `Click when ${names[originalNameIndex]} is asleep`
@@ -64,16 +58,8 @@ describe(`Kid object > putDown() method:`, function () {
       }
       expect(passed).toBe(true);
     });
-    console.log("afterEach() being called?");
   }
-
   for (var originalNameIndex = 0; originalNameIndex < names.length; originalNameIndex++) {
     testPutDown(originalNameIndex);
   }
 });
-//test if order is correct
-//putDown
-//asleepYet
-//takeUp
-//test if new values are correct (including header)
-//test number of children left
